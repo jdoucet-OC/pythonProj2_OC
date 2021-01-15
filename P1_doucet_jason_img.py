12d627e5514d13cc1f0ff3c194559d7591b6a81f
Doucet Jason <jdoucetoc@gmail.com>
2021-01-15 15:47:36 +0100

    Modification création du dossier

--- P1_doucet_jason_img.py
+++ P1_doucet_jason_img.py
@@ -10,11 +10,16 @@ all_csv_files = [file
                  for path, subdir, files in os.walk(PATH)
                  for file in glob(os.path.join(path, EXT))]
 
-col_list=["product_page_url", "universal_ product_code (upc)", "title","product_description"
-                             ,"price_including_tax", "price_excluding_tax", "number_available"
-                             , "category", "review_rating", "image_url"]
+col_list = ["product_page_url", "universal_ product_code (upc)", "title", "product_description",
+            "price_including_tax", "price_excluding_tax", "number_available",
+            "category", "review_rating", "image_url"]
+ii = 1
+if not os.path.exists('images'):
+    os.makedirs('images')
 for link in all_csv_files:
-    df = pd.read_csv(link,usecols=col_list)
-    local_links=(df["image_url"])
+    df = pd.read_csv(link, usecols=col_list)
+    local_links = (df["image_url"])
     for item in local_links:
-        urllib.request.urlretrieve(item, 'images\\'+item.split('/')[-1])
+        urllib.request.urlretrieve(item, 'images/'+item.split('/')[-1])
+        print('Image '+str(ii)+'/500')
+        ii += 1

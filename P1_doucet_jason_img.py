dd070b3e1b007069daaebafe3f63764f0bd2744e
JDO <jdoucetOC@gmail.com>
2021-01-15 09:08:46 +0100

    Fichier permettant l'enregistrement de toutes les images

--- P1_doucet_jason_img.py
+++ P1_doucet_jason_img.py
@@ -4,11 +4,12 @@ from glob import glob
 import pandas as pd
 import urllib.request
 
-PATH = "C:\\Users\\Jason\\.spyder-py3\\git_test\\pythonProj2_OC"
+PATH = os.getcwd()
 EXT = "*.csv"
 all_csv_files = [file
                  for path, subdir, files in os.walk(PATH)
                  for file in glob(os.path.join(path, EXT))]
+
 col_list=["product_page_url", "universal_ product_code (upc)", "title","product_description"
                              ,"price_including_tax", "price_excluding_tax", "number_available"
                              , "category", "review_rating", "image_url"]
@@ -17,3 +18,4 @@ for link in all_csv_files:
     local_links=(df["image_url"])
     for item in local_links:
         urllib.request.urlretrieve(item, 'images\\'+item.split('/')[-1])
+        print('oui')
